f95fe931482e03ae1bddec9327a18d33dae41b33
Amogh-007-Rin <amoghdath233@gmail.com>
2025-12-01 11:41:45 +0000

    Project-setup

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+function myName(name : String){
+    console.log(name)
+}
+
+
+
+function myNumber(num : Number){
+    console.log("Number is :" + num)
+}
+myName("Hi Developer : This is a typescript project");
+myNumber(35);
