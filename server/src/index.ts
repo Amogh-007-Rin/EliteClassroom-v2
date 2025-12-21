9c38c2fb428d27d3c899a0964ec3d7236889c486
Amogh-007-Rin <amoghdath233@gmail.com>
2025-12-21 23:54:30 +0000

    Database-setup-v1

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,25 @@
-import console = require("node:console");
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
-const express = require("express");
+const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
 
-app.get("/", (req : String ,res : any)=>{
-    res.json({
-        message : "Welcome to express server"
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Welcome to express server" });
 });
 
-
+app.get("/classroom/students", async (req: Request, res: Response) => {
+  try {
+    const students = await prisma.student.findMany();
+    console.log("Students", JSON.stringify(students, null, 2));
+    res.json(students);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`)
+  console.log(`Server is running at http://localhost:${port}`);
 });
