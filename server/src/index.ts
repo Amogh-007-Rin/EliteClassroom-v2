5c4e8dba133493d2851805d7075b7dc6a972d63b
Amogh-007-Rin <amoghdath233@gmail.com>
2025-12-26 00:09:38 +0000

    Prisma-Client-Issue-Solved

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,38 +1,72 @@
-
-import { PrismaClient } from "@prisma/client/extension";
 import express from "express";
-// import {Request,Response} from "express";
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 
-const prisma = new PrismaClient()
-async function createTeacher(id : String, email : String, name : String){
-    const newTeacher = await prisma.teacher.create({
-        data: {
-            id,
-            email,
-            name
-        }
+import { prisma } from './prisma.js'
+
+async function main() {
+  // Create a new user with a post
+  const user = await prisma.user.create({
+    data: {
+      name: 'Aliceaaaaaaa',
+      email: 'alice@prisma.iokkaa',
+      posts: {
+        create: {
+          title: 'Hello Worldaaaa',
+          content: 'This is aamy first postaaaa!',
+          published: true,
+        },
+      },
+    },
+    include: {
+      posts: true,
+    },
+  })
+  console.log('Created user:', user)
+
+  // Fetch all users with their posts
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  })
+  console.log('All users:', JSON.stringify(allUsers, null, 2))
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/", (req : any, res: any) => {
+    res.json({
+        message : "server is running ",
+        condition : "all set"
     });
-    console.log(newTeacher);
-};
-
-app.get("/teacher/create", async(req : any, res : any) => {
-
-    createTeacher("2332233", "amoghdath233@gmail.com", "amogh");
-    res.json({
-        message : "Teacher Created successfully"
-    })
-
 });
 
-app.get("/", (req : any, res : any) => {
-    res.json({
-        message : "All good, Server is up"
-    })
-});
-
-app.listen(PORT, ()=> {
-    console.log(`Server Is Running At http://localhost:${PORT}`);
-});
+app.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
