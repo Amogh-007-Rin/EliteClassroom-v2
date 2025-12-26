da4ef17ef9fc825b977af7a74b2cc778dbf13934
Amogh-007-Rin <amoghdath233@gmail.com>
2025-12-26 15:16:08 +0000

    Added-multiple-backend-files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,48 +1,9 @@
 import express from "express";
+import { prisma } from './prisma.js'
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 
-import { prisma } from './prisma.js'
-
-async function main() {
-  // Create a new user with a post
-  const user = await prisma.user.create({
-    data: {
-      name: 'Aliceaaaaaaa',
-      email: 'alice@prisma.iokkaa',
-      posts: {
-        create: {
-          title: 'Hello Worldaaaa',
-          content: 'This is aamy first postaaaa!',
-          published: true,
-        },
-      },
-    },
-    include: {
-      posts: true,
-    },
-  })
-  console.log('Created user:', user)
-
-  // Fetch all users with their posts
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  })
-  console.log('All users:', JSON.stringify(allUsers, null, 2))
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
 
 
 
